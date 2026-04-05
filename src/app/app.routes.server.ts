f7db7968af61e8e7a81189118d63c35133a1a8e5
Chandan Shaw <chandan.shaw@bassetti-group.com>
2026-04-05 23:07:28 +0530

    Increase bundle warning; add server route

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'problems/:slug',
+    renderMode: RenderMode.Server, // Tell Angular: "Don't try to build this at compile time"
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  googleOAuthRedirectUrl: 'https://dsadrill.duckdns.org/oauth2/authorization/google',
+  apiUrl: 'https://dsa-drill.duckdns.org/api',
+  googleOAuthRedirectUrl: 'https://dsa-drill.duckdns.org/oauth2/authorization/google',
 };
