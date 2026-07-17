a28b40c49b4176ccef66fa35e40eb17937987ee4
Chandan Shaw <dev.chandan.shaw@gmail.com>
2026-07-17 09:00:17 +0530

    fix: thrid party cookies issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dsa-drill.duckdns.org/api',
+  apiUrl: '/api',
   googleOAuthRedirectUrl: 'https://dsa-drill.duckdns.org/login/oauth2/code/google',
 };
